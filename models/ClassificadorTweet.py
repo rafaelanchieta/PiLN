c59f9a455789ff2aaa9e27c33fbd04ef158de0e5
jezielcm <jezielcm@gmail.com>
2021-05-28 22:26:46 -0300

    Modelos salvos

---
+++ models/ClassificadorTweet.py
@@ -0,0 +1,14 @@
+import pickle
+
+class ClassificadorTweet():
+    def __init__(self):
+        with open('models/ModeloTweetsTF-IDF.pickle', 'rb') as file_pickle:
+            self.clf = pickle.loads(file_pickle.read())
+            file_pickle.close()
+    
+    def predict(self, texto):
+        if self.clf.predict([texto]):
+            return 'Irony'
+        else:
+            return 'Not irony'
+
